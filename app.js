06aa4d001e37ef9d2d3e46bf3cfe5543f1ac3c27
Zack Roessler <zk.roess@gmail.com>
2023-01-09 14:05:26 -0800

    finished practice interview

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 const ageToVote = 18
+const currentYear = 2023
+const jsReleaseYear = 1995
+
+console.log("The current age of JS is", currentYear-jsReleaseYear)
+if (currentYear-jsReleaseYear >= ageToVote) {
+  console.log("Javascript is old enough to vote.")
+} else {
+  console.log("Javascript is not old enough to vote.")
+}
+console.log("Starting...")
+for (let i = 0; i <= 100; ++i) {
+  if (i % 10 == 0) {
+    console.log(i)
+    if (i == 50) {
+      console.log("Halfway")
+    }
+  }
+}
+console.log("Finished.")
 
 const user = {
   name: 'Sherlock Holmes',
@@ -7,6 +26,7 @@ const user = {
     city: 'London'
   }
 }
+console.log(`${user.name} lives at ${user.address.street}, ${user.address.city}`)
 
 const cases = [
   { title: 'The Hound of the Baskervilles', releaseYear: 1901 },
@@ -16,6 +36,13 @@ const cases = [
   { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 }
 ]
 
-// YOUR JS CODE
+// for (const index in cases) {
+//   console.log(`${cases[index].title}, ${cases[index].releaseYear}`)
+// }
+for (const index in cases) {
+  if (cases[index].releaseYear == 1903) {
+    console.log(`${cases[index].title}`)
+  }
+}
 
 
